29bd5d089e8ba2840d2a136c32ca4257c8261efd
anhtran <anhtr077@gmail.com>
2024-10-28 12:55:39 -0700

    update original file

--- activity/original.py
+++ activity/original.py
@@ -1,2 +1,25 @@
 def uncommon_from_sentences(s1, s2):
-    pass
+    #take result as diction to store
+    s1_words = s1.split(" ")
+    s2_words = s2.split(" ")
+    
+    # print(s1)
+    
+    words_dict = {}
+    uncommon_list = []
+    
+    for word in s1_words:
+        if word not in s2:
+            words_dict[word] = 1
+            
+    for word in s2_words:
+        if word not in s1:
+            words_dict[word] = 1
+            
+    for word in words_dict:
+        uncommon_list.append(word)
+            
+    return uncommon_list
+            
+
+print(uncommon_from_sentences("this apple is sweet", "this apple is sour" ))
